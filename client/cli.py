feaf4c4c9d66da5878aa6826a62585064a0bcaf5
Andrey Melchikhin <amelchikhin@yandex.ru>
2023-05-04 18:13:06 +0300

    release version (client and server with threading)

--- client/cli.py
+++ client/cli.py
@@ -2,7 +2,8 @@ import threading
 import socket
 from cli_logger import cli_log
 import json
-from file_partition import read_full_message
+import time
+
 
 
 class Client:
@@ -10,15 +11,29 @@ class Client:
         self.host = ip_addr
         self.port = port
         self.sock = None
+        # Создаем новое подключение и авторизуемся
+        self.connect()
+        self.route_menu()
+        # Создаем поток на чтение сообщений с сервера
+        read_thr = threading.Thread(target=self.read_message, daemon=True)
+        read_thr.start()
+        write_trh = threading.Thread(target=self.send_message, daemon=True)
+        write_trh.start()
+
+
+
 
     def connect(self):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        """Создание подключения по указанному порту и адресу"""
+        sock = socket.socket()
+        sock.setblocking(True)
         sock.connect((self.host, self.port))
-        self.sock = sock
         cli_log.info(f"Подключен к серверу {self.host}:{self.port}")
-        self.sock.settimeout(1)
+        self.sock = sock
 
     def reg_form(self):
+        """Регистрация пользователя в системе"""
+        print("Регистрация нового пользователя --->")
         while True:
             username = input("Введите имя пользователя: ")
             password = input("Введите пароль: ")
@@ -26,80 +41,114 @@ class Client:
             if password == password2 and username != "":
                 data = {"username": username, "password": password}
                 self.sock.sendall(json.dumps(data).encode())
-                server_response = self.sock.recv(1024).decode()
-                if server_response == "success":
-                    print("Клиент успешно зарегистрирован")
+                cli_log.info(f"Отправлен запрос на регистрацию пользователя {username}")
+                resp = self.sock.recv(1024).decode()
+                server_response = json.loads(resp)
+                cli_log.info(f"Принимаем ответ от сервера - результат регистрации: {server_response}")
+                if server_response['text']['result'] == "success":
+                    print("Клиент успешно зарегистрирован! Теперь можете авторизоваться --->")
+                    self.auth_form()
                     cli_log.info(f"Пользователь {username} зарегистрирован на сервере.")
                     break
                 elif not server_response:
                     cli_log.info("Сервер не отвечает. Попробуйте позже.")
+                    self.connect()
+                    self.reg_form()
                     break
                 else:
                     print("Пользователь с таким именем уже существует")
                     cli_log.info(f"Пользователь с таким именем уже существует")
+                    self.reg_form()
                     continue
 
     def auth_form(self):
+        """Авторизация пользователя в системе"""
+        print("Авторизуйтесь в системе используя логин и пароль --->")
         while True:
             username = input("Введите имя пользователя: ")
             password = input("Введите пароль: ")
             data = {"username": username, "password": password}
             if username != "" and password != "":
-                self.sock.sendall(json.dumps(data).encode())
-                server_response = self.sock.recv(1024).decode()
-                if server_response == "success":
+                self.sock.sendall(json.dumps(data).encode('utf-8'))
+                cli_log.info(f"Отправлен запрос на авторизацию пользователя {username}")
+                server_response = json.loads(self.sock.recv(1024).decode('utf-8'))
+                cli_log.info(f"Принимаем ответ от сервера - результат авторизации: {server_response}")
+                if server_response['text']['result'] == "success":
                     print("Клиент успешно авторизован")
                     cli_log.info(f"Пользователь {username} авторизован на сервере.")
-                    break
-                elif server_response == "wrong pass":
-                    print("Неверный пароль")
+                    self.send_message(username)
+                elif server_response['text']['result'] == "wrong pass":
+                    print("Неверный пароль!")
                     cli_log.info(f"Пользователь {username} ввел неверный пароль.")
-                    self.connect()
-                    continue
-                elif server_response == "not registered":
+                    self.auth_form()
+                elif server_response['text']['result'] == "not registered":
                     print("Пользователь не зарегистрирован. Пожалуйста, зарегистрируйтесь.")
+                    cli_log.info(f"Пользователь {username} не зарегистрирован на сервере.")
                     self.reg_form()
-                    break
+
                 elif not server_response:
                     cli_log.info("Сервер не отвечает. Попробуйте позже.")
+                    self.connect()
+
+                else:
+                    cli_log.info(f"Получен неожидаенный ответ от сервера: {server_response}")
                     break
             else:
-                print("Поля не должны быть пустыми")
-                continue
+                print("Поля ввода не должны быть пустыми!")
 
     def read_message(self):
-        while True:
-            try:
-                data = json.loads(read_full_message(self.sock).decode())
-                username, message = data["username"], data["text"]
-                print(f"Сообщение от {username}: {message}")
-                data = ''
-            except IOError:
-                cli_log.info("Сервер не отвечает. Попробуйте позже.")
-                break
+        """Чтение сообщений с сервера"""
+        try:
+            data = json.loads(self.sock.recv(1024).decode('utf-8'))
+            username, message = data["username"], data["text"]
+            print(f"Сообщение от {username}: {message}")
+            cli_log.info(f"Принято сообщение от {username} : *{message}*")
+        except IOError:
+            cli_log.info("Сервер не отвечает. Попробуйте позже.")
 
-    def send_message(self):
+    def send_message(self, username):
         while True:
-            message = input()
+            message = input("Введите сообщение ---> ")
             if message == "exit":
                 self.sock.close()
                 cli_log.info("Пользователь вышел из чата.")
                 break
-            data = {"username": "user", "text": message}
-            self.sock.sendall(json.dumps(data).encode())
+
+            data = {'username': username, 'text': message}
+            data = json.dumps(data)
+            self.sock.sendall(data.encode('utf-8'))
             cli_log.info(f"Пользователь отправил сообщение: {message}")
+            self.read_message()
             data = ''
 
+    def route_menu(self):
+        """Меню выбора действия"""
+        while True:
+            print("Выберите действие:")
+            print("1. Авторизация")
+            print("2. Регистрация")
+            print("3. Выход")
+            choice = input("Введите номер действия: ")
+            if choice == "1":
+                self.sock.sendall(choice.encode('utf-8'))
+                self.auth_form()
+            elif choice == "2":
+                self.sock.sendall(choice.encode('utf-8'))
+                self.reg_form()
+                self.auth_form()
+            elif choice == "3":
+                self.sock.close()
+                break
+            else:
+                print("Неверный ввод")
+                continue
+
+
 def main():
-    client = Client('localhost', 7777)
-    client.connect()
-    client.auth_form()
-    read_thread = threading.Thread(target=client.read_message, daemon=True)
-    read_thread.start()
-    client.send_message()
+    port_input = int(input("Введите порт подключения: "))
+    ip_addr_input = input("Введите ip-адрес подключения к серверу: ")
+    client = Client(ip_addr_input, port_input)
+
 
 if __name__ == "__main__":
     main()
-
-
-
--- server/server.py
+++ server/server.py
@@ -3,11 +3,11 @@ import threading
 import json
 import uuid
 import datetime
-from typing import Dict, Union
 from serv_logger import server_logger
 from auth import UserRegistration
 from port_checker import PortValidator
 from settings import *
+import time
 
 
 class Server:
@@ -18,18 +18,20 @@ class Server:
         self.sessions_list = []
         self.auth_processing = UserRegistration()
         self.socket_init()
+        print("Сервер запущен на порту:", port)
         server_logger.info(f"Инициализация сервера. Слушает порт:{port}")
 
         while True:
             conn, address = self.sock.accept()
-            token, est_time = self.generate_token()
-            server_logger.info(f"Создан временный токен для клиента {address[0]}")
-            self.sessions_list.append((conn, address[0], token, est_time))
             server_logger.info(f"Подключился клиент {address[0]}")
-            thr = threading.Thread(target=self.message_logic, args=(conn, address[0]), daemon=True)
+
+            thr = threading.Thread(target=self.route(conn, address), args=(conn, address), daemon=True)
             thr.start()
+            for session in self.sessions_list:
+                self.check_token(session)
 
     def socket_init(self):
+        """Инициализация подключения"""
         sock = socket.socket()
         sock.bind(("", self.port))
         sock.listen(0)
@@ -37,129 +39,133 @@ class Server:
 
     @staticmethod
     def generate_token():
-        token = uuid.uuid4()
+        """Генерация токена для поддержания сессии пользователя"""
+        token = str(uuid.uuid4())
         est_time = datetime.datetime.now() + datetime.timedelta(minutes=TOKEN_EST_TIME)
         return token, est_time
 
-    def check_session(self):
-        for session in self.sessions_list:
-            if session[3] < datetime.datetime.now():
-                self.sessions_list.remove(session)
-                self.auth_logic(session[0], session[1])
-                self.generate_token()
-            else:
-                continue
+    def check_token(self, session):
+        """Проверка токена пользователя"""
+        active = self.sock.getsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE)
+        if session[4] < datetime.datetime.now() and active is True:
+            server_logger.info(f"Сессия пользователя {session[2]} еще активна, генерирую новый токен")
+            token, est_time = self.generate_token()
+            session[3] = token
+            session[4] = est_time
+            self.auth_processing.update_info(session[1], session[2], 'active', token, est_time)
 
-    @staticmethod
-    def send_message(conn, data: dict, ip: str) -> None:
+        else:
+            self.sessions_list.remove(session)
+            server_logger.info(f"Сессия пользователя {session[2]} истекла, удаляю из списка активных сессий")
+            self.auth_processing.update_info(session[1], session[2], 'offline', '', '')
+            self.auth_logic(session[0], session[1])
+
+
+    def send_message(self, conn, data: dict, ip: str) -> None:
         """Отправка данных"""
         data_text = data
         if type(data) == dict:
             data = json.dumps(data, ensure_ascii=False)
-        data = data.encode()
+        data = data.encode('utf-8')
         conn.send(data)
         server_logger.info(f"Сообщение {data_text} было отправлено клиенту {ip}")
 
     def message_logic(self, conn, ip_addr):
+        """Логика обработки сообщений"""
         data = ""
         while True:
             conn_data = conn.recv(4096)
-            data += conn_data.decode()
-            if "LF" in data:
-                username = self.auth_processing.get_info(ip_addr)[1]
-                server_logger.info(
-                    f"Получили сообщение {data} от клиента {ip_addr} ({username})"
-                )
-                data = {"username": username, "text": data}
-                server_logger.info(
-                    f"Текущее кол-во подключений к серверу: {len(self.sessions_list)}"
-                )
-                for conn in self.sessions_list:
-                    current_conn, current_ip = conn[0], conn[1]
-                    self.check_session()
-                    try:
-                        self.send_message(current_conn, data, current_ip)
-                    except BrokenPipeError:
-                        server_logger.info(f"Клиент {current_ip} отключился")
-                        continue
-                data = ""
-            else:
-                server_logger.info(f"Приняли часть данных от клиента {ip_addr}: '{data}'")
+            data = json.loads(conn_data.decode('utf-8'))
+            username = data["username"]
+            server_logger.info(
+                f"Получили сообщение {data['text']} от клиента {ip_addr} ({username})"
+            )
+            data = {"username": username, "text": data['text']}
+            server_logger.info(
+                f"Текущее кол-во подключений к серверу: {len(self.sessions_list)}"
+            )
+            for connection in self.sessions_list:
+                current_conn, current_ip = connection[0], connection[1]
+                try:
+                    self.send_message(current_conn, data, current_ip)
+                except BrokenPipeError:
+                    server_logger.info(f"Клиент {current_ip} отключился")
+                    self.auth_processing.update_info(current_ip, username, 'offline', '', '')
+                    self.sessions_list.remove(connection)
+                    continue
+            data = ""
             if not conn_data:
+                self.auth_processing.update_info(ip_addr, username, 'offline', '', '')
                 break
 
     def reg_logic(self, conn, addr):
-        data = json.loads(conn.recv(1024).decode())
+        data = json.loads(conn.recv(1024).decode('utf-8'))
         username = data["username"]
         password = data["password"]
         addr = addr[0]
         if self.auth_processing.userreg(addr, username, password):
             server_logger.info(f"Пользователь {username} зарегистрирован (ip: {addr})")
-            conn.sendall("success".encode())
+            data = {'username': username, 'text': {'result': 'success'}}
         else:
             server_logger.info(f"Пользователь c ip: {addr} уже зарегистрирован")
-            conn.sendall("failure".encode())
-            self.auth_logic(conn, addr)
+            data = {'username': username, 'text': {'result': 'failure'}}
+
+        self.send_message(conn, data, addr)
+        server_logger.info(f"Отправлены данные о результате регистсрации клиенту {addr}")
 
     def auth_logic(self, conn, addr):
-        data = json.loads(conn.recv(1024).decode())
+        data = json.loads(conn.recv(1024).decode('utf-8'))
         username = data["username"]
         password = data["password"]
+        auth_result = self.auth_processing.userauth(username, password)
         addr = addr[0]
-        if self.auth_processing.userauth(addr, username, password) == 1:
+        if auth_result == 1:
             server_logger.info(f"Пользователь {username} авторизован (ip: {addr})")
-            conn.sendall("success".encode())
-        elif self.auth_processing.userauth(addr, username, password) == 0:
+            data = {'username': username, 'text': {'result': 'success'}}
+            token, est_time = self.generate_token()
+            self.auth_processing.update_info(addr, username, 'active', token, est_time)
+            server_logger.info(f"Создан временный токен для клиента {addr}: {token} (время жизни: {est_time})")
+            self.sessions_list.append((conn, addr, username, token, est_time))
+            self.send_message(conn, data, addr)
+            server_logger.info(f"Клиенту {addr} отправлена информация {data} о результате авторизации")
+
+
+        elif auth_result == 0:
             server_logger.info(f"Пользователь {username} ввел неверный пароль (ip: {addr})")
-            conn.sendall("wrong pass".encode())
+            data = {'username': username, 'text': {'result': "wrong pass"}}
+            self.send_message(conn, data, addr)
+            server_logger.info(f"Клиенту {addr} отправлена информация {data} о результате авторизации")
+            self.auth_logic(conn, addr)
         else:
             server_logger.info(f"Пользователь {username} не зарегистрирован (ip: {addr})")
-            conn.sendall("not registered".encode())
+            data = {'username': username, 'text': {'result': "not registered"}}
+            self.send_message(conn, data, addr)
+            server_logger.info(f"Клиенту {addr} отправлена информация о результате авторизации")
             self.reg_logic(conn, addr)
 
-    def handle_logic(self, conn, addr):
-        while True:
-            addr = addr[0]
-            conn.sendall("Выберите действие: reg - регистрация,\
-                          auth - авторизация,\
-                          exit - выход".encode()
-                         )
-            data = conn.recv(1024).decode()
-            if data == "exit":
-                break
-            elif data == "reg":
-                self.reg_logic(conn, addr)
-            elif data == "auth":
-                self.auth_logic(conn, addr)
-            else:
-                conn.sendall("Неверный ввод".encode())
-
-    def client_handle(self, conn, addr):
-        ip = addr[0]
-        if self.auth_processing.get_info(ip)[3] != 'logged in':
-            self.auth_logic(conn, ip)
-        elif not self.auth_processing.get_info(ip):
-            self.reg_logic(conn, ip)
+    def route(self, conn, addr):
+        client_ip = addr[0]
+        menu_resp = json.loads(conn.recv(1024).decode('utf-8'))
+        if menu_resp == 1:
+            self.auth_logic(conn, addr)
+            self.message_logic(conn, client_ip)
+        elif menu_resp == 2:
+            self.reg_logic(conn, addr)
+            self.auth_logic(conn, addr)
+            self.message_logic(conn, client_ip)
         else:
+            server_logger.info(f"Клиент {client_ip} отключился")
             conn.close()
+            exit(0)
 
 
 def main():
     port_input = input("Введите номер порта для подключения -> ")
     server_logger.info(f"Проверка порта {port_input}, выбранного пользователем для подключения.")
-    # Тут проверка на то, занят ли порт:
-    try:
-        validator = PortValidator()
-        free_port = validator.port_validation(int(port_input))
-        server = Server(free_port)
+    validator = PortValidator()
+    free_port = validator.port_validation(int(port_input))
+    server = Server(free_port)
 
-    except ValueError:
-        server_logger.error(f"Порт {port_input} не валиден.")
-        print("Порт не валиден.")
-    else:
-        free_port = validator.port_validation(DEFAULT_PORT)
-        server_logger.info(f"Выставлен дефолтный порт {free_port}.")
-        server = Server(free_port)
 
 
 if __name__ == "__main__":
